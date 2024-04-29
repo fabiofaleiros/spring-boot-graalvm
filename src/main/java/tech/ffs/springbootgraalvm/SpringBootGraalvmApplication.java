c1c759ebfe837a8a21c9c0e9cec3012d7a2f99ca
Fábio Siqueira <fabio.siqueira@axians.com>
2024-04-29 16:40:44 +0100

    initial commit

---
+++ src/main/java/tech/ffs/springbootgraalvm/SpringBootGraalvmApplication.java
@@ -0,0 +1,13 @@
+package tech.ffs.springbootgraalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraalvmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ffs/springbootgraalvm/SpringBootGraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ffs.springbootgraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraalvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
